625b723556d1f22b23ccd56696679d8ddf7001ba
raghavpatidar <raghavpatidar997764@gmail.com>
2023-12-21 16:35:02 +0530

    Create Contacts

---
+++ src/main/java/com/practice/AddressBook.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    ArrayList<Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        this.contacts.add(contact);
+    }
+
+    public void viewContacts() {
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
+
+}
